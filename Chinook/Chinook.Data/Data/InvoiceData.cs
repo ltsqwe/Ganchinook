46d845b8ecda55571fa9ba634579c99dcef2ce4c
ltswer <ltswer@gmail.com>
2019-07-12 11:16:15 +0900

    implement required methods

--- Chinook/Chinook.Data/Data/InvoiceData.cs
+++ Chinook/Chinook.Data/Data/InvoiceData.cs
@@ -1,8 +1,30 @@
+using System;
+using System.Collections.Generic;
 using MyLibrary;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace Chinook.Data
 {
     public class InvoiceData : EntityData<Invoice>
     {
+        public Invoice GetByPk(int invoiceId)
+        {
+            using (ChinookEntities context = DbContextFactory.Create<ChinookEntities>())
+            {
+                return context.Invoices.FirstOrDefault(x => x.InvoiceId == invoiceId);
+            }
+        }
+
+        public override List<Invoice> GetAll(Expression<Func<Invoice, bool>> predicate = null)
+        {
+            using (ChinookEntities context = DbContextFactory.Create<ChinookEntities>())
+            {
+                var query = from x in context.Invoices
+                    select x;
+
+                return query.ToList();
+            }
+        }
     }
 }
--- Chinook/Chinook.Data/Data/InvoiceLineData.cs
+++ Chinook/Chinook.Data/Data/InvoiceLineData.cs
@@ -1,8 +1,46 @@
+using System;
+using System.Collections.Generic;
 using MyLibrary;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace Chinook.Data
 {
     public class InvoiceLineData : EntityData<InvoiceLine>
     {
+        public InvoiceLine GetByPk(int invoiceLineId)
+        {
+            using (ChinookEntities context = DbContextFactory.Create<ChinookEntities>())
+            {
+                return context.InvoiceLines.FirstOrDefault(x => x.InvoiceLineId == invoiceLineId);
+            }
+        }
+
+        public override List<InvoiceLine> GetAll(Expression<Func<InvoiceLine, bool>> predicate = null)
+        {
+            using (ChinookEntities context = DbContextFactory.Create<ChinookEntities>())
+            {
+                var query = from x in context.InvoiceLines
+                    select x;
+
+                return query.ToList();
+            }
+        }
+        
+        public List<Invoice> GetInvoices(int invoiceLineId)
+        {
+            using (ChinookEntities context = DbContextFactory.Create<ChinookEntities>())
+            {
+                var invoiceId =  context.InvoiceLines.FirstOrDefault(x => x.InvoiceLineId == invoiceLineId)?.InvoiceId;
+                if (!invoiceId.HasValue)
+                    return null;
+
+                var query = from x in context.Invoices
+                    where x.InvoiceId == invoiceId
+                    select x;
+
+                return query.ToList();
+            }
+        }
     }
 }
